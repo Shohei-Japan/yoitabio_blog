e3c2b2d20d8a11d3d48489c8588e4480fbb56f6d
Shohei-Japan <kai.entertainment0304@gmail.com>
2020-01-14 02:32:30 +0900

    fix config

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -2,9 +2,11 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const GRAPHCMS_API = process.env.GRAPHCMS_API
+console.log(GRAPHCMS_API)
 
 export default () => ({
   link: new HttpLink({ uri: GRAPHCMS_API }),
   cache: new InMemoryCache(),
-  defaultHttpLink: false
+  defaultHttpLink: false,
+  httpEndpoint: GRAPHCMS_API
 })
